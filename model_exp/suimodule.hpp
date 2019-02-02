1cf9299b707626deb8688d5cca9725226188c39a
Haleigh Wright <hmw@Haleighs-MacBook-Pro.local>
2019-02-02 15:19:42 -0800

    add haleighs files

---
+++ model_exp/suimodule.hpp
@@ -0,0 +1,14 @@
+//
+//  suimodule.hpp
+//  
+//
+//  Created by Haleigh Wright on 1/30/19.
+//
+
+#ifndef suimodule_hpp
+#define suimodule_hpp
+
+
+double SUIpathLoss(double f, double TxH, double RxH, double d, int mode);
+
+#endif /* suimodule_hpp */
